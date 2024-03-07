eff61237eafea98784307e36bff8ab9a4d02c71a
bohokasmakkara <162553789+bohokasmakkara@users.noreply.github.com>
2024-03-07 11:32:09 +0200

    Create ft_str_is_printable.c
    Create a function that returns 1 if the string given as a parameter contains only printable characters, and 0 if it contains any other character.

---
+++ ft_str_is_printable.c
@@ -0,0 +1,17 @@
+int	ft_str_is_printable(char *str)
+{
+	int	i;
+
+	if (str[0] == '\0')
+	{
+		return (1);
+	}
+	i = 0;
+	while (str[i] != 0)
+	{
+		if (!(str[i] >= 32) && (str[i] <= 127))
+			return (0);
+		i++;
+	}
+	return (1);
+}
