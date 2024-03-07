703905178898765855631d2f12e14c102b09d1d4
bohokasmakkara <162553789+bohokasmakkara@users.noreply.github.com>
2024-03-07 11:30:22 +0200

    Create ft_str_is_lowercase.c
    Create a function that returns 1 if the string given as a parameter contains only lowercase alphabetical characters, and 0 if it contains any other character.

---
+++ ft_str_is_lowercase.c
@@ -0,0 +1,18 @@
+int	ft_str_is_lowercase(char *str)
+
+{
+	int	i;
+
+	if (str[0] == '\0')
+	{
+		return (1);
+	}
+	i = 0;
+	while (str[i] != 0)
+	{
+		if (!(str[i] >= 97) && (str[i] <= 122))
+			return (0);
+		i++;
+	}
+	return (1);
+}
